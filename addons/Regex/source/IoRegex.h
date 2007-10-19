5feea4f37a4a5294a9c29f97ba6d1ee2ac143103
Daniel Rosengren <danne.rosengren@gmail.com>
2007-10-19 15:41:42 +0800

    Redesigned/rewrote Regex
    * Split Regex up into Regex and RegexMatches. These aren't meant to be cloned
      and used directly. You create new instances by calling methods on Sequence.
      See addons/Regex/io/Sequence_regex.io.
    * Made Regex immutable.
    * A number of other changes/additions/improvements. Look at the unit tests
      to see what the API is like.

--- addons/Regex/source/IoRegex.h
+++ addons/Regex/source/IoRegex.h
@@ -7,8 +7,8 @@ docLicense("BSD revised")
 #define IOREGEX_DEFINED 1
 
 #include "IoObject.h"
-#include "IoSeq.h"
-#include <pcre.h>
+#include "IoMap.h"
+#include "Regex.h"
 
 #define ISREGEX(self) IoObject_hasCloneFunc_(self, (IoTagCloneFunc *)IoRegex_rawClone)
 
@@ -16,71 +16,48 @@ typedef IoObject IoRegex;
 
 typedef struct
 {
-	pcre *regex;
-	pcre_extra *studyData;
-	int *outputVector;
-	int outputVectorSize;
-	int captureCount;
 	IoSymbol *pattern;
-	IoSymbol *string;
-	IoObject *currentMatch;
-	int lastIndex;
-	int previousCompileOptions;
-	int compileOptions;
-	int execOptions;
-	int matchGroupSize;
+	int options;
+	IoMap *nameToIndexMap;
+	Regex *regex;
 } IoRegexData;
 
 IoRegex *IoRegex_rawClone(IoRegex *self);
 IoRegex *IoRegex_proto(void *state);
 IoRegex *IoRegex_new(void *state);
-
+IoRegex *IoRegex_newWithPattern_(void *state, IoSymbol *pattern);
 void IoRegex_free(IoRegex *self);
 void IoRegex_mark(IoRegex *self);
 
+Regex *IoRegex_rawRegex(IoRegex *self);
+
 /* ------------------------------------------------------------------------------------------------*/
 
-IoObject *IoRegex_setPattern(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_with(IoRegex *self, IoObject *locals, IoMessage *m);
+
 IoObject *IoRegex_pattern(IoRegex *self, IoObject *locals, IoMessage *m);
-
-IoObject *IoRegex_setString(IoRegex *self, IoObject *locals, IoMessage *m);
-IoObject *IoRegex_string(IoRegex *self, IoObject *locals, IoMessage *m);
-
-IoObject *IoRegex_nextMatch(IoRegex *self, IoObject *locals, IoMessage *m);
-IoObject *IoRegex_currentMatch(IoRegex *self, IoObject *locals, IoMessage *m);
-IoObject *IoRegex_resetSearch(IoRegex *self, IoObject *locals, IoMessage *m);
 IoObject *IoRegex_captureCount(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_nameToIndexMap(IoRegex *self, IoObject *locals, IoMessage *m);
 
 IoObject *IoRegex_version(IoRegex *self, IoObject *locals, IoMessage *m);
 
 /* ------------------------------------------------------------------------------------------------*/
-/* Compile options */
+/* Options */
 
-IoObject *IoRegex_anchoredOn(IoRegex *self, IoObject *locals, IoMessage *m);
-IoObject *IoRegex_anchoredOff(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_caseless(IoRegex *self, IoObject *locals, IoMessage *m);
+IoRegex *IoRegex_notCaseless(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_isCaseless(IoRegex *self, IoObject *locals, IoMessage *m);
 
-IoObject *IoRegex_caselessOn(IoRegex *self, IoObject *locals, IoMessage *m);
-IoRegex *IoRegex_caselessOff(IoRegex *self, IoObject *locals, IoMessage *m);
-
-IoObject *IoRegex_dotAllOn(IoRegex *self, IoObject *locals, IoMessage *m);
-IoRegex *IoRegex_dotAllOff(IoRegex *self, IoObject *locals, IoMessage *m);
-
+IoObject *IoRegex_dotAll(IoRegex *self, IoObject *locals, IoMessage *m);
+IoRegex *IoRegex_notDotAll(IoRegex *self, IoObject *locals, IoMessage *m);
 IoObject *IoRegex_isDotAll(IoRegex *self, IoObject *locals, IoMessage *m);
-IoRegex *IoRegex_setIsDotAll(IoRegex *self, IoObject *locals, IoMessage *m);
 
-IoObject *IoRegex_extendedOn(IoRegex *self, IoObject *locals, IoMessage *m);
-IoRegex *IoRegex_extendedOff(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_extended(IoRegex *self, IoObject *locals, IoMessage *m);
+IoRegex *IoRegex_notExtended(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_isExtended(IoRegex *self, IoObject *locals, IoMessage *m);
 
-IoObject *IoRegex_multilineOn(IoRegex *self, IoObject *locals, IoMessage *m);
-IoRegex *IoRegex_multilineOff(IoRegex *self, IoObject *locals, IoMessage *m);
-
-IoObject *IoRegex_UTF8On(IoRegex *self, IoObject *locals, IoMessage *m);
-IoRegex *IoRegex_UTF8Off(IoRegex *self, IoObject *locals, IoMessage *m);
-
-/* ------------------------------------------------------------------------------------------------*/
-/* Match options */
-
-IoObject *IoRegex_matchEmptyOn(IoRegex *self, IoObject *locals, IoMessage *m);
-IoObject *IoRegex_matchEmptyOff(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_multiline(IoRegex *self, IoObject *locals, IoMessage *m);
+IoRegex *IoRegex_notMultiline(IoRegex *self, IoObject *locals, IoMessage *m);
+IoObject *IoRegex_isMultiline(IoRegex *self, IoObject *locals, IoMessage *m);
 
 #endif
