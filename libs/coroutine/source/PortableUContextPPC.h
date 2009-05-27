0ea6c1e7d1538269b336512760d53e5c32a9053c
Steve Dekorte <steve@StevesLaptop-7.local>
2009-05-26 19:00:35 -0700

    PortableUContextPPC cleanup

--- libs/coroutine/source/PortableUContextPPC.h
+++ libs/coroutine/source/PortableUContextPPC.h
@@ -4,7 +4,7 @@ This software was developed as part of a project at MIT.
  Copyright (c) 2005-2006 Russ Cox,
  Massachusetts Institute of Technology
 
- Permission is hereby granted, io_free of charge, to any person obtaining
+ Permission is hereby granted, free of charge, to any person obtaining
  a copy of this software and associated documentation files (the
  "Software"), to deal in the Software without restriction, including
  without limitation the rights to use, copy, modify, merge, publish,
