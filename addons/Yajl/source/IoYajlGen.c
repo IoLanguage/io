c99d0dea1425ef0d2a83824d27f2db45b9aace97
Steve Dekorte <steve@StevesLaptop-7.local>
2009-11-03 20:05:56 -0800

    added rest of YajlGen methods

--- addons/Yajl/source/IoYajlGen.c
+++ addons/Yajl/source/IoYajlGen.c
@@ -25,28 +25,7 @@ IoTag *IoYajlGen_newTag(void *state)
 	return tag;
 }
 
-IoYajlGen *IoYajlGen_proto(void *state)
-{
-	IoYajlGen *self = IoObject_new(state);
-	IoObject_tag_(self, IoYajlGen_newTag(state));
 
-	yajl_gen_config config = { 0, "" };
-	IoObject_setDataPointer_(self, yajl_gen_alloc(&config, NULL));
-
-	IoState_registerProtoWithFunc_(state, self, IoYajlGen_proto);
-
-	{
-		IoMethodTable methodTable[] = 
-		{
-			{"generate", IoYajlGen_generate},
-			{"pushString", IoYajlGen_pushString},
-			{NULL, NULL},
-		};
-		IoObject_addMethodTable_(self, methodTable);
-	}
-		
-	return self;
-}
 
 IoYajlGen *IoYajlGen_rawClone(IoYajlGen *proto)
 {
@@ -71,6 +50,12 @@ void IoYajlGen_free(IoYajlGen *self)
 	yajl_gen_free(DATA(self));
 }
 
+IoObject *IoYajlGen_pushNull(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	yajl_gen_null(DATA(self));
+	return self;
+}
+
 IoObject *IoYajlGen_pushString(IoYajlGen *self, IoObject *locals, IoMessage *m)
 {
 	IoSeq *s = IoMessage_locals_seqArgAt_(m, locals, 0);
@@ -79,6 +64,63 @@ IoObject *IoYajlGen_pushString(IoYajlGen *self, IoObject *locals, IoMessage *m)
 		(const unsigned char *)IOSYMBOL_BYTES(s), 
 		IOSYMBOL_LENGTH(s));
 
+	return self;
+}
+
+IoObject *IoYajlGen_pushInteger(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	int i = IoMessage_locals_intArgAt_(m, locals, 0);
+	yajl_gen_integer(DATA(self), i);
+	return self;
+}
+
+IoObject *IoYajlGen_pushDouble(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	double d = IoMessage_locals_doubleArgAt_(m, locals, 0);
+	yajl_gen_double(DATA(self), d);
+	return self;
+}
+
+IoObject *IoYajlGen_pushNumberString(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	IoSeq *s = IoMessage_locals_seqArgAt_(m, locals, 0);
+	
+	yajl_gen_number(DATA(self), 
+		(const  char *)IOSYMBOL_BYTES(s), 
+		IOSYMBOL_LENGTH(s));
+		
+	return self;
+}
+
+IoObject *IoYajlGen_pushBool(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	int b = IoMessage_locals_boolArgAt_(m, locals, 0);
+	yajl_gen_bool(DATA(self), b);
+	return self;
+}
+
+
+IoObject *IoYajlGen_openMap(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	yajl_gen_map_open(DATA(self));
+	return self;
+}
+
+IoObject *IoYajlGen_closeMap(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	yajl_gen_map_close(DATA(self));
+	return self;
+}
+
+IoObject *IoYajlGen_openArray(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	yajl_gen_array_open(DATA(self));
+	return self;
+}
+
+IoObject *IoYajlGen_closeArray(IoYajlGen *self, IoObject *locals, IoMessage *m)
+{
+	yajl_gen_array_close(DATA(self));
 	return self;
 }
 
@@ -96,4 +138,36 @@ IoObject *IoYajlGen_generate(IoYajlGen *self, IoObject *locals, IoMessage *m)
 	IoObject_setDataPointer_(self, yajl_gen_alloc(&config, NULL));
 	
     return out;  
+}
+
+IoYajlGen *IoYajlGen_proto(void *state)
+{
+	IoYajlGen *self = IoObject_new(state);
+	IoObject_tag_(self, IoYajlGen_newTag(state));
+
+	yajl_gen_config config = { 0, "" };
+	IoObject_setDataPointer_(self, yajl_gen_alloc(&config, NULL));
+
+	IoState_registerProtoWithFunc_(state, self, IoYajlGen_proto);
+
+	{
+		IoMethodTable methodTable[] = 
+		{
+			{"pushNull", IoYajlGen_pushNull},
+			{"pushString", IoYajlGen_pushString},
+			{"pushInteger", IoYajlGen_pushInteger},
+			{"pushDouble", IoYajlGen_pushDouble},
+			{"pushNumberString", IoYajlGen_pushNumberString},
+			{"pushBool", IoYajlGen_pushBool},
+			{"openMap", IoYajlGen_openMap},
+			{"closeMap", IoYajlGen_closeMap},
+			{"openArray", IoYajlGen_openArray},
+			{"closeArray", IoYajlGen_closeArray},
+			{"generate", IoYajlGen_generate},
+			{NULL, NULL},
+		};
+		IoObject_addMethodTable_(self, methodTable);
+	}
+		
+	return self;
 }
