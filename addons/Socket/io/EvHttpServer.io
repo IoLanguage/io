68c7c68bba4a3384d06ac7a30a411a711fd28069
Steve Dekorte <steve@StevesLaptop-7.local>
2009-12-18 20:05:44 -0800

    EvHttpServer request and response headers working

--- addons/Socket/io/EvHttpServer.io
+++ addons/Socket/io/EvHttpServer.io
@@ -4,6 +4,7 @@ EvHttpServer do(
 	port ::= 80
 	
 	request := Object clone do(
+		headers := Map clone
 		uri := nil
 		postData := nil
 		parse := method(
@@ -20,22 +21,23 @@ EvHttpServer do(
 	)
 	
 	response := Object clone do(
+		headers := Map clone
 		statusCode := 200
 		data := ""
 	)
 	
 	handleRequestCallback := method(
 		//writeln("HttpServer handleRequest not implemented 1")
+		response headers := Map clone
 		request parse
 		//writeln("parameters = ", request parameters keys)
-		//self handleRequest(request)
+		self handleRequest(request)
 		
 		self
 	)
 	
 	handleRequest := method(request,
 		writeln("HttpServer handleRequest not implemented")
-		
 		response data := ""
 		response statusCode := 200
 	)
--- addons/Socket/source/IoEvHttpServer.c
+++ addons/Socket/source/IoEvHttpServer.c
@@ -8,6 +8,7 @@
 #include "IoState.h"
 #include "IoNumber.h"
 #include "IoSeq.h"
+#include "IoMap.h"
 #include <event.h>
 
 #define HTTP_SERVERERROR 500
@@ -75,6 +76,131 @@ void IoEvHttpServer_free(IoEvHttpServer *self)
 	IoEvHttpServer_rawStop(self);
 }
 
+void IoEvHttpServer_readRequestHeaders(IoEvHttpServer *self, struct evhttp_request *req)
+{	
+	IoObject *request  = IoObject_getSlot_(self, IOSYMBOL("request"));
+	//IOASSERT(!ISNIL(request), "nil request");
+	assert(!ISNIL(request));
+	
+	IoMap *headers  = IoObject_getSlot_(request, IOSYMBOL("headers"));
+	//IOASSERT(ISMAP(headers), "request headers not a map");
+	assert(ISMAP(headers));
+	
+	const char *headerNames[] = {
+		"Accept-Ranges",
+		"Age",
+		"Allow",
+		"Cache-Control",
+		"Content-Encoding",
+		"Content-Language",
+		"Content-Length",
+		"Content-Location",
+		"Content-Disposition",
+		"Content-MD5",
+		"Content-Range",
+		"Content-Type",
+		"Date",
+		"ETag",
+		"Expires",
+		"Last-Modified",
+		"Location",
+		"Server",
+		"Set-Cookie", 
+		0x0
+	};
+	
+	{
+		//struct evkeyvalq *h = req->input_headers;
+		int i = 0;
+		const char *name;
+		
+		//IoMap_rawEmpty(headers);
+		
+		while ((name = headerNames[i]))
+		{
+			const char *value = evhttp_find_header(req->input_headers, name);
+			
+			if (value) 
+			{
+				//printf("response header: %s : %s\n", name, value);
+				IoMap_rawAtPut(headers, IOSYMBOL(name), IOSYMBOL(value));
+			}
+			else 
+			{
+				IoMap_rawAtPut(headers, IOSYMBOL(name), IONIL(self)); // replace with IoMap_rawEmpty
+			}
+
+			
+			i ++;
+		}
+	}
+}
+
+void IoEvHttpServer_writeResponseHeaders(IoEvHttpServer *self, struct evhttp_request *req)
+{	
+	IoObject *response  = IoObject_getSlot_(self, IOSYMBOL("response"));
+	//IOASSERT(!ISNIL(response), "nil request");
+	assert(!ISNIL(response));
+	
+	IoMap *headers  = IoObject_getSlot_(response, IOSYMBOL("headers"));
+	//IOASSERT(ISMAP(headers), "request headers not a map");
+	assert(ISMAP(headers));
+	
+	const char *headerNames[] = {
+		"Accept-Ranges",
+		"Age",
+		"Allow",
+		"Cache-Control",
+		"Content-Encoding",
+		"Content-Language",
+		"Content-Length",
+		"Content-Location",
+		"Content-Disposition",
+		"Content-MD5",
+		"Content-Range",
+		"Content-Type",
+		"Date",
+		"ETag",
+		"Expires",
+		"Last-Modified",
+		"Location",
+		"Pragma",
+		"Proxy-Authenticate",
+		"Refresh",
+		"Retry-After",
+		"Server",
+		"Set-Cookie",
+		"Trailer",
+		"Transfer-Encoding",
+		"Vary",
+		"Via",
+		"Warning",
+		"WWW-Authenticate",
+		0x0
+	};
+	
+	{
+		//struct evkeyvalq *h = req->input_headers;
+		int i = 0;
+		const char *name;
+		
+		//IoMap_rawEmpty(headers);
+		
+		while ((name = headerNames[i]))
+		{
+			IoSeq *v = IoMap_rawAt(headers, IOSYMBOL(name));
+			
+			if (v && ISSEQ(v)) 
+			{
+				evhttp_remove_header(req->output_headers, name);
+				evhttp_add_header(req->output_headers, name, CSTRING(v));
+			}
+
+			i ++;
+		}
+	}
+}
+
 void IoEvHttpServer_handleRequest(struct evhttp_request *req, void *arg)
 {
 	IoEvHttpServer *self = arg;
@@ -87,25 +213,34 @@ void IoEvHttpServer_handleRequest(struct evhttp_request *req, void *arg)
 	
 	IoObject_setSlot_to_(request, IOSYMBOL("uri"), IOSYMBOL(uri));
 	IoObject_setSlot_to_(request, IOSYMBOL("postData"), postData);
-
+	IoEvHttpServer_readRequestHeaders(self, req);
+	
 	if (strcmp(uri, "/favicon.ico") == 0)
 	{
 		evhttp_send_reply(req, HTTP_OK, HTTP_OK_MESSAGE, buf);
 	}
 	else
-	{		
+	{
 		IoObject *response = IoObject_getSlot_(self, IOSYMBOL("response"));
 		
 		//IoObject_perform(self, self, IOSYMBOL("handleRequestCallback"));
 		IoMessage *m = IoMessage_newWithName_label_(IOSTATE, IOSYMBOL("handleRequestCallback"), IOSYMBOL("IoEvHttpServer"));
 		IoMessage_locals_performOn_(m, self, self);
 
+		IoEvHttpServer_writeResponseHeaders(self, req);
+		
 		IoSeq *data = IoObject_seqGetSlot_(response, IOSYMBOL("data"));
 		int statusCode = IoObject_doubleGetSlot_(response, IOSYMBOL("statusCode"));
 
 		buf = evbuffer_new();
 		evbuffer_add_printf(buf, "%s", CSTRING(data));
 
+/*
+		IoMap *responseHeaders = IoObject_seqGetSlot_(response, IOSYMBOL("headers"));
+		evhttp_remove_header(req->output_headers, k);
+		evhttp_add_header(req->output_headers, k, v);
+
+*/
 		if (statusCode == 200)
 		{
 			evhttp_send_reply(req, HTTP_OK, HTTP_OK_MESSAGE, buf);
