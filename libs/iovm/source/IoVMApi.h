f11a601c4b7644e73b1372f66fb03a80f588eedb
U-ATTICCAT\Jim <Jim@atticcat.(none)>
2010-02-10 14:22:29 -0500

    correct changes preventing MSVC build

--- libs/iovm/source/IoVMApi.h
+++ libs/iovm/source/IoVMApi.h
@@ -1,15 +1,15 @@
 #ifndef IOVM_API_H_
 #define IOVM_API_H_
 
-//#if !defined(__MINGW32__) && defined(WIN32)
-//#if defined(BUILDING_IOVM_DLL) || defined(BUILDING_IOVMALL_DLL)
-//#define IOVM_API __declspec(dllexport)
-//#else
-//#define IOVM_API __declspec(dllimport)
-//#endif
+#if !defined(__MINGW32__) && defined(WIN32)
+#if defined(BUILDING_IOVM_DLL) || defined(BUILDING_IOVMALL_DLL)
+#define IOVM_API __declspec(dllexport)
+#else
+#define IOVM_API __declspec(dllimport)
+#endif
 
-//#else
+#else
 #define IOVM_API
-//#endif
+#endif
 
 #endif
