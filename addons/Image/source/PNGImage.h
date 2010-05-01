41fe3bea01401f0848f2e3e0063375e5dd66a421
Petr Man <petr@madnetwork.org>
2010-05-01 02:13:52 +0200

    Fixed Image addons. It is now compatible with libpng14.

--- addons/Image/source/PNGImage.h
+++ addons/Image/source/PNGImage.h
@@ -12,6 +12,11 @@
  *  PNGImage instance after freeing its external byte array.
  */
 
+// these two defines have been depricated since libpng10 in favor of NULL
+// makes Image addon compatible with libpng12 and libpng14
+#define png_infopp_NULL (png_infopp)NULL
+#define int_p_NULL (int*)NULL
+
 #ifndef PNGIMAGE_DEFINED
 #define PNGIMAGE_DEFINED 1
 
