5e1a00196c39f1bcda006b5884465e01089e6572
Jeremy Tregunna <jeremy.tregunna@me.com>
2010-06-06 19:12:07 -0400

    Enhanced build.sh to be used as a stop-gap for those who want linkInstall or one command to build and install.

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 # Build script for Steve
 
-[ -d build ] && rm -rf build && mkdir build
-cd build
-cmake ..
-make install
+INSTALL_PREFIX="/usr/local"
+
+if [ X"$1" = X"linkInstall" ]; then
+	rm -rf $INSTALL_PREFIX/lib/io $INSTALL_PREFIX/bin/io
+	for i in $PWD/build/_build/dll/*; do ln -s $i $INSTALL_PREFIX/lib/`basename $i`
+	mkdir $INSTALL_PREFIX/lib/io
+	ln -s $PWD/addons $INSTALL_PREFIX/lib/io/
+elif [ X"$1" = X"help" ]; then
+	echo "Io build and installation"
+	echo "Usage: $0 [help|linkInstall]"
+	echo "Any argument that isn't help or linkInstall will build and install Io."
+else
+	[ -d build ] && rm -rf build && mkdir build
+	cd build
+	cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" ..
+	make all install
+fi
+
