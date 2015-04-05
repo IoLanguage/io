edd4b02000c14d5d1187f25081a411aefafd7fd0
Perceval FARAMAZ <perfaram@windowslive.com>
2015-04-05 08:43:59 +0200

    Base SMC functions and typdefs

--- addons/AppleExtras/source/AppleSMC.c
+++ addons/AppleExtras/source/AppleSMC.c
@@ -31,3 +31,55 @@ io_connect_t smcGetDataPort(void)
 
 	return smcDataPort;
 }
+
+kern_return_t SMCClose()
+{
+	return IOServiceClose(conn);
+}
+
+kern_return_t SMCCall(int index, SMCKeyData_t *inputStructure, SMCKeyData_t *outputStructure)
+{
+	size_t   structureInputSize;
+	size_t   structureOutputSize;
+	
+	structureInputSize = sizeof(SMCKeyData_t);
+	structureOutputSize = sizeof(SMCKeyData_t);
+	
+	return IOConnectCallStructMethod( conn, index,
+									 // inputStructure
+									 inputStructure, structureInputSize,
+									 // ouputStructure
+									 outputStructure, &structureOutputSize );
+	
+}
+
+kern_return_t SMCReadKey(UInt32Char_t key, SMCVal_t *val)
+{
+	kern_return_t result;
+	SMCKeyData_t  inputStructure;
+	SMCKeyData_t  outputStructure;
+	
+	memset(&inputStructure, 0, sizeof(SMCKeyData_t));
+	memset(&outputStructure, 0, sizeof(SMCKeyData_t));
+	memset(val, 0, sizeof(SMCVal_t));
+	
+	inputStructure.key = _strtoul(key, 4, 16);
+	inputStructure.data8 = SMC_CMD_READ_KEYINFO;
+	
+	result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
+	if (result != kIOReturnSuccess)
+		return result;
+	
+	val->dataSize = outputStructure.keyInfo.dataSize;
+	_ultostr(val->dataType, outputStructure.keyInfo.dataType);
+	inputStructure.keyInfo.dataSize = val->dataSize;
+	inputStructure.data8 = SMC_CMD_READ_BYTES;
+	
+	result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
+	if (result != kIOReturnSuccess)
+		return result;
+	
+	memcpy(val->bytes, outputStructure.bytes, sizeof(outputStructure.bytes));
+	
+	return kIOReturnSuccess;
+}
--- addons/AppleExtras/source/AppleSMC.h
+++ addons/AppleExtras/source/AppleSMC.h
@@ -3,4 +3,51 @@
 #include <IOKit/IOKitLib.h>
 #include <CoreFoundation/CoreFoundation.h>
 
+typedef char		SMCBytes_t[32];
+typedef char		UInt32Char_t[5];
+typedef char		Flag[1];
+typedef UInt		flag;
+typedef UInt16		PWMValue;
+
+typedef struct SMCKeyData_vers_t {
+	char                  major;
+	char                  minor;
+	char                  build;
+	char                  reserved[1];
+	UInt16                release;
+} SMCKeyData_vers_t;
+
+typedef struct {
+	UInt16                version;
+	UInt16                length;
+	UInt32                cpuPLimit;
+	UInt32                gpuPLimit;
+	UInt32                memPLimit;
+} SMCKeyData_pLimitData_t;
+
+typedef struct {
+	UInt32                dataSize;
+	UInt32                dataType;
+	char                  dataAttributes;
+} SMCKeyData_keyInfo_t;
+
+typedef struct {
+	UInt32                  key;
+	SMCKeyData_vers_t       vers;
+	SMCKeyData_pLimitData_t pLimitData;
+	SMCKeyData_keyInfo_t    keyInfo;
+	char                    result;
+	char                    status;
+	char                    data8;
+	UInt32                  data32;
+	SMCBytes_t              bytes;
+} SMCKeyData_t;
+
+typedef struct {
+	UInt32Char_t            key;
+	UInt32                  dataSize;
+	UInt32Char_t            dataType;
+	SMCBytes_t              bytes;
+} SMCVal_t;
+
 io_connect_t smcGetDataPort(void);
