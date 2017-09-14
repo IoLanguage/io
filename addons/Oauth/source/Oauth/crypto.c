27d3900e8a88d0ee45773666a5a257ca735eb0b0
Pietro Cerutti <gahr@gahr.ch>
2017-09-14 12:43:32 +0000

    Unbreak building on 32-bit with clang

--- addons/Oauth/source/Oauth/crypto.c
+++ addons/Oauth/source/Oauth/crypto.c
@@ -658,7 +658,7 @@ void sha1_finalize(struct sha1_context *self, unsigned char digest[20])
 	sha1_update(self, bits, 8);
 
 	/* Store state in digest */
-	SHA1Encode(digest, (uint32_t *)self->state, 20);
+	SHA1Encode(digest, self->state, 20);
 
 	/* Zeroize sensitive information.
 	 */
--- addons/Oauth/source/Oauth/crypto.h
+++ addons/Oauth/source/Oauth/crypto.h
@@ -16,7 +16,7 @@ struct md5_context
 
 struct sha1_context
 {
-	unsigned long state[5];
+	uint32_t state[5];
 	unsigned long count[2];
 	unsigned char buffer[64];
 };
