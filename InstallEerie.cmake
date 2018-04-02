3e7d81526ea39af7d5eb362591313cedf5f17864
Ales Tsurko <ales.tsurko@gmail.com>
2018-04-02 22:12:06 +0300

    prototype installing phase for Eerie

---
+++ InstallEerie.cmake
@@ -0,0 +1,5 @@
+set(WITHOUT_EERIE 0 CACHE BOOL "Don't install Eerie")
+if(NOT WITHOUT_EERIE)
+    # TODO install Eerie here
+    message("------------Installing Eerie------------")
+endif(NOT WITHOUT_EERIE)
