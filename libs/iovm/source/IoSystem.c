08106a0beb3afea980dd4b6086a7c1ea7c6ff86d
Ales Tsurko <ales.tsurko@gmail.com>
2018-06-09 21:49:07 +0300

    fix System installPrefix on Windows

--- libs/iovm/source/IoSystem.c
+++ libs/iovm/source/IoSystem.c
@@ -88,8 +88,8 @@ IO_METHOD(IoObject, installPrefix)
 	if( GetModuleFileName( NULL, acPath, 256 ) != 0)
 	{
 		// guaranteed file name of at least one character after path
-		strcpy(( strrchr( acPath, '\\' ) ), "\\..\\");
-		_fullpath(root,acPath,256);
+		strcpy(strrchr(acPath, '\\'), "\\.");
+		_fullpath(root, acPath, 256);
 	}
 
 	return IoState_symbolWithCString_(IOSTATE, root);
