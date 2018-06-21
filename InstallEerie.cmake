9de46b13203ca056f427cca7f577861b6cd846c7
Ales Tsurko <ales.tsurko@gmail.com>
2018-06-21 16:12:09 +0300

    run ldconfig after install on linux

--- InstallEerie.cmake
+++ InstallEerie.cmake
@@ -1,6 +1,10 @@
+IF(UNIX AND NOT APPLE)
+    execute_process(COMMAND ldconfig)
+ENDIF(UNIX AND NOT APPLE)
+
 message("------------Installing Eerie------------")
 IF(WIN32)
     execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/io ${CMAKE_SOURCE_DIR}/setup.io)
 ELSE()
     execute_process(COMMAND ${CMAKE_INSTALL_PREFIX}/bin/io ${CMAKE_SOURCE_DIR}/setup.io)
-ENDIF()
+ENDIF(WIN32)
